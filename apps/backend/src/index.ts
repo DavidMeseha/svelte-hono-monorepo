4ed5eaf695ef47c391e92ecc151ebd81e6837d3b
David <davidmmyh@gmail.com>
2025-08-23 10:14:49 +0300

    monorepo-ecommerce-svelte-hono

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,33 @@
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { routes } from "./routes/index.js";
+import { createProduct } from "./db/createProduct.js";
+import { products } from "./db/products-setup.js";
+import { serveStatic } from "hono/bun";
+import { logger } from "hono/logger";
+
+const app = new Hono();
+
+app.use(
+  "/*",
+  cors({
+    origin: process.env.ORIGIN ?? "",
+  })
+);
+
+app.use("*", logger());
+app.use("/images/*", serveStatic({ root: "./public" }));
+
+app.get("/init", async (c) => {
+  const x = await createProduct(products[0]);
+  const y = await createProduct(products[1]);
+  return c.json({ products: [{ ...x }, { ...y }] });
+});
+
+app.route("/api", routes);
+
+app.notFound((c) => {
+  return c.json({ error: "Route not found" }, 404);
+});
+
+export default app
