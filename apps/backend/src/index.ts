d3e02dbeb7b28713eed6eabfe2dc4c4258bb5d55
David <davidmmyh@gmail.com>
2025-08-26 17:30:42 +0300

    Fix: build and added prisma generate before building

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -34,7 +34,6 @@ app.route('/api', routes);
 
 const port = parseInt(process.env.PORT || '3000');
 serve({ fetch: app.fetch, port }, (info) => {
-	console.log(`Server running at http://localhost:${info.port}`);
 	console.info(info);
 });
 
